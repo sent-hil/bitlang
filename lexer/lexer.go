6e86716bf2329ccc27666fb5612d1cbf0e5703a7
Senthil Arivudainambi <senthil@walkerandcobrands.com>
2017-02-13 08:19:54 -0800

    lexer: add Lexer type and NewLexer() initializer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,29 @@
 package lexer
+
+import (
+	"bufio"
+	"io"
+)
+
+type Lexer struct {
+	runes []rune
+}
+
+func NewLexer(reader io.Reader) (*Lexer, error) {
+	runes := []rune{}
+	bufReader := bufio.NewReader(reader)
+
+	for {
+		c, _, err := bufReader.ReadRune()
+		if err != nil && err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		runes = append(runes, c)
+	}
+
+	return &Lexer{runes: runes}, nil
+}
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,10 +1,19 @@
 package lexer
 
 import (
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestLexer(t *testing.T) {
+	Convey("NewLexer", t, func() {
+		Convey("It parses and stores characters to be iterated", func() {
+			l, err := NewLexer(strings.NewReader("Hello"))
+			So(err, ShouldBeNil)
+			So(len(l.runes), ShouldEqual, 5)
+			So(l.runes, ShouldResemble, []rune{'H', 'e', 'l', 'l', 'o'})
+		})
+	})
 }
