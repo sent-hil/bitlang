d4a0b4691d68ce3f49a00a233ac81c54ff134def
Senthil Arivudainambi <senthil@walkerandcobrands.com>
2017-02-21 18:14:38 -0800

    lexer: remove old read/peek methods now that there's runeio

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,10 +1,9 @@
 package lexer
 
 import (
-	"bufio"
 	"errors"
-	"io"
-	"unicode/utf8"
+
+	"github.com/sent-hil/bitlang/runeio"
 )
 
 var (
@@ -13,60 +12,9 @@ var (
 )
 
 type Lexer struct {
-	runes []rune
+	reader *runeio.Reader
 }
 
-func NewLexer(reader io.Reader) (*Lexer, error) {
-	runes := []rune{}
-	bufReader := bufio.NewReader(reader)
-
-	for {
-		c, _, err := bufReader.ReadRune()
-		if err != nil && err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		runes = append(runes, c)
-	}
-
-	return &Lexer{runes: runes}, nil
-}
-
-func (l *Lexer) PeekRunes(size int) ([]rune, error) {
-	if size < 0 {
-		return nil, ErrNegativeCount
-	}
-	if size > len(l.runes) {
-		return nil, ErrBoundsExceeded
-	}
-
-	return l.runes[:size], nil
-}
-
-func (l *Lexer) ReadRune() (rune, error) {
-	if len(l.runes) == 0 {
-		return utf8.RuneError, io.EOF
-	}
-
-	char := l.runes[0]
-	l.runes = l.runes[1:]
-
-	return char, nil
-}
-
-func (l *Lexer) ReadRunes(size int) ([]rune, error) {
-	if size < 0 {
-		return nil, ErrNegativeCount
-	}
-	if size > len(l.runes) {
-		return nil, ErrBoundsExceeded
-	}
-
-	runes := l.runes[0:size]
-	l.runes = l.runes[size:]
-
-	return runes, nil
+func NewLexer(reader *runeio.Reader) (*Lexer, error) {
+	return &Lexer{reader: reader}, nil
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,103 +1,16 @@
 package lexer
 
 import (
-	"io"
 	"strings"
 	"testing"
 
+	"github.com/sent-hil/bitlang/runeio"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestLexer(t *testing.T) {
 	Convey("Lexer", t, func() {
-		l, err := NewLexer(strings.NewReader("Hello"))
+		_, err := NewLexer(runeio.NewReader(strings.NewReader("Hello")))
 		So(err, ShouldBeNil)
-
-		Convey("NewLexer", func() {
-			Convey("It parses and stores chars to be iterated", func() {
-				So(len(l.runes), ShouldEqual, 5)
-				So(l.runes, ShouldResemble, []rune{'H', 'e', 'l', 'l', 'o'})
-			})
-		})
-
-		Convey("Peak", func() {
-			Convey("It peeks into stored chars when given size is within stored chars size", func() {
-				chars, err := l.PeekRunes(2)
-				So(err, ShouldBeNil)
-				So(chars, ShouldResemble, []rune{'H', 'e'})
-			})
-
-			Convey("It peeks entire stored chars slice", func() {
-				chars, err := l.PeekRunes(5)
-				So(err, ShouldBeNil)
-				So(chars, ShouldResemble, []rune{'H', 'e', 'l', 'l', 'o'})
-			})
-
-			Convey("It returns error when give size is greater than stored chars size", func() {
-				_, err := l.PeekRunes(6)
-				So(err, ShouldEqual, ErrBoundsExceeded)
-			})
-
-			Convey("It returns error when give size is negative", func() {
-				_, err := l.PeekRunes(-1)
-				So(err, ShouldEqual, ErrNegativeCount)
-			})
-		})
-
-		Convey("ReadRune", func() {
-			Convey("It returns chars as they're read from chars slice", func() {
-				// required since l.Read modifies l.runes
-				dupRunes := make([]rune, len(l.runes))
-				copy(dupRunes, l.runes)
-
-				for i := 0; i < len(dupRunes); i++ {
-					char, err := l.ReadRune()
-					So(err, ShouldBeNil)
-
-					// cast to string for readable errors
-					So(string(char), ShouldEqual, string(dupRunes[i]))
-				}
-
-				Convey("It returns EOF if no chars are left in chars slice", func() {
-					char, err := l.ReadRune()
-					So(err, ShouldEqual, io.EOF)
-					So(char, ShouldEqual, '\uFFFD') // error rune
-				})
-			})
-		})
-
-		Convey("ReadRunes", func() {
-			Convey("It returns given size of chars when size is within stored chars size", func() {
-				chars, err := l.ReadRunes(1)
-				So(err, ShouldBeNil)
-				So(chars, ShouldResemble, []rune{'H'})
-			})
-
-			Convey("It returns entire stored chars slice", func() {
-				chars, err := l.ReadRunes(5)
-				So(err, ShouldBeNil)
-				So(chars, ShouldResemble, []rune{'H', 'e', 'l', 'l', 'o'})
-			})
-
-			Convey("It removes returned chars from stored chars slice", func() {
-				_, err := l.ReadRunes(1)
-				So(err, ShouldBeNil)
-				So(l.runes, ShouldResemble, []rune{'e', 'l', 'l', 'o'})
-
-				chars, err := l.ReadRunes(1)
-				So(err, ShouldBeNil)
-				So(chars, ShouldResemble, []rune{'e'})
-			})
-
-			Convey("It returns error when give size is greater than stored chars size", func() {
-				_, err := l.ReadRunes(6)
-				So(err, ShouldEqual, ErrBoundsExceeded)
-			})
-
-			Convey("It returns error when give size is negative", func() {
-				_, err := l.ReadRunes(-1)
-				So(err, ShouldEqual, ErrNegativeCount)
-			})
-		})
 	})
 }
